bd2ef6a4131d37786fb87284ee06e8364561a9f2
Jordan Green <ST10083222@vcconnect.edu.za>
2023-08-28 22:31:03 +0200

    Add project files.

---
+++ Controllers/TicketController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MVCSendEmail.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                // Send email using SmtpClient
+                using (SmtpClient smtpClient = new SmtpClient())
+                {
+                    // Configure the SMTP client settings
+                    smtpClient.UseDefaultCredentials = true;
+                    smtpClient.Host = "smtp.gmail.com";
+                    smtpClient.Port = 587;
+                    smtpClient.EnableSsl = true;
+                    smtpClient.Credentials = new System.Net.NetworkCredential("Your Gmail Account", "Add Generated Password");
+
+                    // Create the email message
+                    MailMessage mailMessage = new MailMessage();
+                    mailMessage.From = new MailAddress("Your Gmail Account");
+                    mailMessage.To.Add(ticket.Email);
+                    mailMessage.Subject = "Ticket Information";
+                    mailMessage.Body = $"Name: {ticket.Name}\nModule Code: {ticket.ModuleCode}\nDescription: {ticket.Description}";
+
+                    // Send the email
+                    smtpClient.Send(mailMessage);
+                }
+
+                return RedirectToAction("Create", "Ticket");
+            }
+
+            return View(ticket);
+        }
+    }
+}
---
+++ Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Ticket
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Please enter your name.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Please enter the module code.")]
+    public string ModuleCode { get; set; }
+
+    [Required(ErrorMessage = "Please enter a description.")]
+    public string Description { get; set; }
+
+    [Required(ErrorMessage = "Please enter your email address.")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+    public string Email { get; set; }
+}
